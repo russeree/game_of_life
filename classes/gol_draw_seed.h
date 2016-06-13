e44a1917f635f2c5a758384a78792d9313749ac7
russeree <reese.russell@ymail.com>
2016-06-13 02:49:11 -0700

    Added Drawing Seed

---
+++ classes/gol_draw_seed.h
@@ -0,0 +1,19 @@
+/***
+ * Creator: Reese Russell
+ * Class: GolDrawSeed
+ * Desc: A class to open a window and draw a seed
+ ***/
+
+#ifndef GAME_OF_LIFE_SEED_DRAW
+#define GAME_OF_LIFE_SEED_DRAW
+
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/menuitem,h>
+
+class GolDrawSeed : public Gtk::Window
+{
+
+};
+#endif
