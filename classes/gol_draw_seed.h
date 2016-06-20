3310edfe946cf123e7bce009251efd4664968002
russeree <reese.russell@ymail.com>
2016-06-19 21:36:51 -0700

    Put the cairo object in a drawing area

--- classes/gol_draw_seed.h
+++ classes/gol_draw_seed.h
@@ -44,18 +44,32 @@ namespace enumGolDrawSeedDebug
 // Main Class: Game of Life Drawing seed.
 class GolDrawSeed : public Gtk::Window
 {
+    int x = 1337;
+    // Game of life Drawing Seed Class
+    protected:
+    class SeedDrawingArea : public Gtk::DrawingArea
+    {
+        public:
+            GolDrawSeed *gds_p;
+            SeedDrawingArea(GolDrawSeed *gol_draw_seed);
+            virtual ~SeedDrawingArea();
+            bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+    };
     public:
         GolDrawSeed();
         virtual ~GolDrawSeed();
-    // Class Configuration
+    // Class Parametes
     protected:
         int status;
         int debug;
+        unsigned int window_size_x;
+        unsigned int window_size_y;
     // Magiks
-    Glib::ustring window_title = "Game of Life Seed Drawing Editor";
+        Glib::ustring window_title = "Game of Life Seed Drawing Editor";
     // Window Layout
     protected:
         Gtk::VBox *main_layout;
+        Gtk::Box *drawing_container;
         Gtk::Box *exit_methds_container;
     // Seed Visualization
         int seed_img_height;
@@ -63,9 +77,8 @@ class GolDrawSeed : public Gtk::Window
         int seed_img_channels;
         guint8 *seed_img_raw_buf;
         Glib::RefPtr<Gdk::Pixbuf> seed_img;
-        Gtk::DrawingArea *visual_seed_dw;
-    // Seed Visualization Functions
-        bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+    // Game of life Drawing Seed Class
+        SeedDrawingArea *visual_seed_dw;
     // Debug Functions
         guint8 *debug_image_gen (unsigned int x_size, unsigned int y_size, unsigned int channels);
 };
--- classes/source/gol_draw_seed.cc
+++ classes/source/gol_draw_seed.cc
@@ -12,6 +12,9 @@
 // Game of Life Seed Drawing  Constructor
 GolDrawSeed::GolDrawSeed()
 {
+    // Setup Window Size
+    this -> window_size_x = 512;
+    this -> window_size_y = 512;
     // Set the default class states
     this -> status = (int)enumGolDrawSeedStatus::initialized;
     this -> debug = (int)enumGolDrawSeedDebug::none;
@@ -19,13 +22,7 @@ GolDrawSeed::GolDrawSeed()
     this -> seed_img_height = 512;
     this -> seed_img_width = 512;
     this -> seed_img_channels = 3;
-    // Construct the drawing window.
-    this -> visual_seed_dw = new Gtk::DrawingArea;
-    this -> main_layout = new Gtk::VBox;
-    this -> exit_methds_container = new Gtk::Box;
-    this -> set_title (this -> window_title);
-    this -> set_default_size(512, 512);
-    this -> add (*visual_seed_dw);
+    // Consturct the seed Image
     this -> seed_img_raw_buf = debug_image_gen(seed_img_width, seed_img_height, seed_img_channels);
     this -> seed_img = Gdk::Pixbuf::create_from_data
         (
@@ -35,8 +32,16 @@ GolDrawSeed::GolDrawSeed()
             8,
             seed_img_width,
             seed_img_height,
-            (seed_img_width)
+            seed_img_width
         );
+    // Construct the drawing window.
+    this -> visual_seed_dw = new SeedDrawingArea(this);
+    this -> main_layout = new Gtk::VBox;
+    this -> exit_methds_container = new Gtk::Box;
+    this -> drawing_container = new Gtk::Box;
+    this -> set_title (this -> window_title);
+    this -> set_default_size(window_size_x, window_size_y);
+    this -> add (*visual_seed_dw);
     // Show the widgets
     this -> show_all();
 }
@@ -47,6 +52,7 @@ GolDrawSeed::~GolDrawSeed()
     delete this -> exit_methds_container;
     delete this -> main_layout;
     delete this -> visual_seed_dw;
+    delete this -> seed_img_raw_buf;
 }
 
 guint8 *GolDrawSeed::debug_image_gen (unsigned int x_size, unsigned int y_size, unsigned int channels)
@@ -65,12 +71,13 @@ guint8 *GolDrawSeed::debug_image_gen (unsigned int x_size, unsigned int y_size, 
 
         }
     }
+
     return image;
 }
 
-bool GolDrawSeed::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+bool GolDrawSeed::SeedDrawingArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
 {
-    if (!seed_img)
+    if (!gds_p -> seed_img)
     {
         return false;
     }
@@ -78,8 +85,21 @@ bool GolDrawSeed::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     const int width = allocation.get_width();
     const int height = allocation.get_height();
     // Draw the image in the middle of the drawing area
-    Gdk::Cairo::set_source_pixbuf(cr, seed_img, 0, 0);
+    Gdk::Cairo::set_source_pixbuf(cr, gds_p -> seed_img, 0, 0);
     cr -> paint();
 
     return true;
 }
+
+// Seed Drawing Area Constructor
+GolDrawSeed::SeedDrawingArea::SeedDrawingArea(GolDrawSeed *gol_draw_seed)
+{
+    // Set this to be of the type game of life drawing seed poiter
+    this -> gds_p = gol_draw_seed;
+}
+// Seed Drawing Area Deconstuctor
+GolDrawSeed::SeedDrawingArea::~SeedDrawingArea()
+{
+}
+
+
