33aca0ed9605d5629fda4ee458ad0ce3fdff520f
russeree <reese.russell@ymail.com>
2016-06-25 02:36:02 -0700

    Late night commit: Saving Point

--- classes/source/gtkmm_pix_buf_ez_mem.cc
+++ classes/source/gtkmm_pix_buf_ez_mem.cc
@@ -16,12 +16,10 @@ GtkmmPixBufEzMem::~GtkmmPixBufEzMem()
 {
 }
 
+// This function is not memory safe if you exceed the boundries of the array please use safely
 void GtkmmPixBufEzMem::write_pix(guint8 *image, guint8 *pix_data, unsigned int channels, unsigned int loc_x, unsigned int loc_y, unsigned int rowstride, unsigned int array_size)
 {
     guint8 *pixel_arr_loc = image + (loc_y * rowstride) + (loc_x * channels);
-    // std::cout << "The Location of the Pixel Buffer is " << &pixel_arr_loc << '\n';
-    // std::cout << "The max size of the array is "<< &image + array_size << '\n';
-    // Now write the pixel locations
     for(unsigned int i = 0; i < channels; i++)
     {
         pixel_arr_loc[i] = pix_data[i];
