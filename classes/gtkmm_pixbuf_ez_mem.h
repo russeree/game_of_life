c7cfc81e79c8a0f4a280c41154ac19aa690c4d6c
russeree <reese.russell@ymail.com>
2016-06-24 23:11:01 -0700

    Added and EZ Pix Class

---
+++ classes/gtkmm_pixbuf_ez_mem.h
@@ -0,0 +1,24 @@
+/***
+ * Creator: Reese Russell
+ * Class: GtkmmPixbufEzMem
+ * Desc: Ez and Fast Pixel Drawing Methods for GTKMM::Pixbuf
+ ***/
+
+#ifndef GTKMM_PIXBUF_EZ_MEM
+#define GTKMM_PIXBUF_EZ_MEM
+
+// Include the Pixbuf Type to Gain Access to the GTKMM Pixbuf types
+
+#include <gdkmm/pixbuf.h>
+
+class GtkmmPixbufEzMem
+{
+    // Con/de(structors)
+    public:
+        GtkmmPixbufEzMem();
+        virtual ~GtkmmPixbufEzMem();
+    public:
+        //Static  Write a pixel to an 1d array of guint8
+        static int write_3_chnl_pix(guint8 *image, unsigned int loc_x, unsigned int loc_y, unsigned int array_size, guint8 red, guint8 green, guint8 blue);
+};
+#endif
---
+++ classes/source/gtkmm_pix_buf_ez_mem.cc
@@ -0,0 +1,23 @@
+/***
+ * Creator: Reese Russell
+ * Class: GtkmmPixbufEzMem
+ * Desc: Ez and Fast pixel writing to a Gtkmm::Pixbuf
+ ***/
+
+#include "gtkmm_pixbuf_ez_mem.h"
+
+// Constructor
+GtkmmPixbufEzMem::GtkmmPixbufEzMem()
+{
+}
+
+// Deconsturctor
+GtkmmPixbufEzMem::~GtkmmPixbufEzMem()
+{
+}
+
+int GtkmmPixbufEzMem::write_3_chnl_pix(guint8 *image, unsigned int loc_x, unsigned int loc_y, unsigned int array_size, guint8 red, guint8 green, guint8 blue)
+{
+    int status = 0;
+    return status;
+}
