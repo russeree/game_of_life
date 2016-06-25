143cf0f097a11fff4ead2e2d35ca8f50b6e092bd
russeree <reese.russell@ymail.com>
2016-06-24 23:14:45 -0700

    Testing Varible Assignment

--- classes/source/gtkmm_pix_buf_ez_mem.cc
+++ classes/source/gtkmm_pix_buf_ez_mem.cc
@@ -18,6 +18,7 @@ GtkmmPixbufEzMem::~GtkmmPixbufEzMem()
 
 int GtkmmPixbufEzMem::write_3_chnl_pix(guint8 *image, unsigned int loc_x, unsigned int loc_y, unsigned int array_size, guint8 red, guint8 green, guint8 blue)
 {
-    int status = 0;
+    int status;
+    status = 0;
     return status;
 }
