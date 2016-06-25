18791a6896c1e6c602f3b1f1dcae959d06097867
russeree <reese.russell@ymail.com>
2016-06-24 23:16:42 -0700

    Noted Limitation

--- classes/gtkmm_pixbuf_ez_mem.h
+++ classes/gtkmm_pixbuf_ez_mem.h
@@ -18,7 +18,7 @@ class GtkmmPixbufEzMem
         GtkmmPixbufEzMem();
         virtual ~GtkmmPixbufEzMem();
     public:
-        //Static  Write a pixel to an 1d array of guint8
+        //Static  Write a pixel to an 1d array of guint8: No rowstepp handler yet
         static int write_3_chnl_pix(guint8 *image, unsigned int loc_x, unsigned int loc_y, unsigned int array_size, guint8 red, guint8 green, guint8 blue);
 };
 #endif
